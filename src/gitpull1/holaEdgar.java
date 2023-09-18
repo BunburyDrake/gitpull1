451f76c1c0bbb3b39dda36abab5e702eb4797808
Angel Pichardo <angel@gmail.com>
2023-09-18 08:17:04 -0500

    mas cAMBIOS

--- src/gitpull1/holaEdgar.java
+++ src/gitpull1/holaEdgar.java
@@ -4,7 +4,7 @@ public class holaEdgar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hola edgar csmbiod");
+System.out.println("hola edgar lolillo");
 }
 
 }
